8aa76e73d8aae4e5fc78e68b7c10bab1aa42ec8a
katfernandez22 <60719712+katfernandez22@users.noreply.github.com>
2020-10-05 20:53:06 -0400

    lab

--- main.js
+++ main.js
@@ -22,7 +22,12 @@ const students = [
   "Zipporah",
 ];
 
-
+function nameAndClass() {
+  for (let i = 0; i < students.length; i++) {
+    console.log(`${students[i]} - Web Development`)
+  }
+}
+nameAndClass();
 
 
 
@@ -38,15 +43,11 @@ const students = [
 //rate of human to dog years
 
 
-
-
-
-
-
-
-
-
-
+function calculateDogAge(age, conversion) {
+  let dogYears = age * conversion;
+  console.log(`Your dog is ${dogYears} years old in dog years.`)
+}
+calculateDogAge(2, 4);
 
 //Exercise #3
 //Write a function named calculateSupply that:
@@ -59,11 +60,12 @@ const students = [
 
 
 
-
-
-
-
-
+function calculateSupply(age, amountPerDay) {
+  let maxAge = 98;
+  let totalNeeded = (amountPerDay * 365) * (maxAge - age);
+  let message = 'You will need ' + totalNeeded + ' toilet papers to last you until you are ' + maxAge;
+  console.log(message);
+}
 
 
 //Exercise #4
@@ -78,13 +80,14 @@ const students = [
 // - Pass the radius to the function
 // - Calculate the area based on the radius and output "The area is SOMETHING"
 
-
-
-
-
-
-
-
+function calcGeometry(radius) {
+  let circumference = Math.PI * 2 * radius;
+  console.log("The circumference is " + circumference);
+}
+function calcArea(radius) {
+  let area = Math.PI * radius * radius;
+  console.log("The area is " + area);
+}
 
 
 //Exercise #5
@@ -95,13 +98,27 @@ const students = [
 // - Now store a fahrenheit temperature int a variable
 // - Convert it to celsius and output "SOMETHING degrees Fahrenheit is SOMETHING degrees Celsius"
 
+function celsiusToFahrenheit() {
+  let celsius = 0; let fahr = (celsius * 9 / 5) + 32;
+  console.log(`${celsius} degrees Celsius is ${fahr} degrees Fahrenheit`)
+}
+celsiusToFahrenheit();
 
-
-
-
+function fahrenheitToCelsius() {
+  let fahrenheit = 0; let cel = ((fahrenheit - 32) * 5) / 9;
+  console.log(`${fahrenheit} degrees Celsius is ${cel} degrees Fahrenheit`)
+}
+fahrenheitToCelsius();
 
 
 //Exercise #6
 //Write a function that takes 3 parameters and returns one number, which is
 //the product of the first two numbers raised to the power of the third
 //passing this function 1,2,3 should give you back the answer to (1 * 2)^3
+
+function funMath(par1, par2, par3) {
+  let prod = (par1 * par2);
+  let answer = Math.pow(prod, par3);
+  console.log(answer);
+}
+funMath(1, 2, 3);
